cf135df970499bc1934d92050f8e91581d882ee7
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-06 20:25:01 +0100

    imported and use the useRegister custom hook function return data on registe.tsx file function

--- src/pages/auth/register/register.tsx
+++ src/pages/auth/register/register.tsx
@@ -1,17 +1,26 @@
 
+import useRegister from '../../../custom-hooks/auth/register';
 import './register.css'
 
 
-const register = () => (
-    <div className='signup_container'>
-        <span>Signup</span>
-        <form>
-            <input type="text" placeholder="username" />
-            <input type="text" placeholder="email" />
-            <input type="psw" placeholder="password" />
-            <button type="submit">Signup</button>
-        </form>
-    </div>
-);
+const Register = () => {
+    const { error, loading, registerHandler, setNameHandler,
+        setPasswordHandler, setEmailHandler, setUsernameHandler } = useRegister();
 
-export default register;
+    return (
+        <div className='signup_container'>
+            <span>Signup</span>
+            <form>
+                <input type="text" placeholder="username" 
+                    onChange={(e) => setUsernameHandler(e)} />
+                <input type="text" placeholder="email" 
+                    onChange={(e) => setEmailHandler(e)}/>
+                <input type="psw" placeholder="password" 
+                    onChange={(e) => setPasswordHandler(e)}/>
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
