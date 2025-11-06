ee0673af4181e895729a7c1c157060eda5e790ea
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-06 19:22:58 +0100

    made the connections between my redux store and my root app with the help of react-redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
+
+import store from './state-management/store/store.ts'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
