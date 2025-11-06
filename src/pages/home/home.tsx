ec1ddc21d54007bb8606d17973c278f4c53c96e6
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-06 20:09:51 +0100

    retrieved updated user from createslice with useSelector hook

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,22 +1,26 @@
 import { NavLink } from 'react-router-dom'
 import { MdCreate } from "react-icons/md";
+import { useSelector } from 'react-redux';
 
 import './home.css'
 
 
-const home = () => (
-    <div className='home_container'>
-        <div>
-            <div>Welcome {}!</div>
-            <p>Please click on the right icon to create post 👉</p>
+const Home = () => {
+    const userState = useSelector(u => u.users.user)
+    return (
+        <div className='home_container'>
+            <div>
+                <div>Welcome {userState.name}!</div>
+                <p>Please click on the right icon to create post 👉</p>
+            </div>
+            <div>
+                <NavLink to="/create">
+                    <MdCreate title="Create Post"/>
+                </NavLink>
+                <NavLink to="/create">Create Post</NavLink>
+            </div>
         </div>
-        <div>
-            <NavLink to="/create">
-                <MdCreate title="Create Post"/>
-            </NavLink>
-            <NavLink to="/create">Create Post</NavLink>
-        </div>
-    </div>
-);
+    );
+}
 
-export default home;
+export default Home;
