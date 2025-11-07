3861083a642d2642ad1cfa622bf25a7dcefc1e95
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-07 10:57:01 +0100

    create the post hooks component to perform state and side effects

---
+++ src/custom-hooks/post/create.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+
+
+const useCreatePost = () => {
+    const [title, setTitle] = useState<string>("")
+    const [error, setError] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+    const [description, setDescription] = useState<string>("")
+
+
+    const setTitleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("TITLE", e.target.value)
+        setTitle(e.target.value)
+    }
+
+    const setDescriptionHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        console.log("Description", e.target.value)
+        setDescription(e.target.value)
+    }
+
+    const createPostHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(title.length === 0 && description.length === 0) {
+            return
+        }
+        try {
+            setLoading(true)
+            const data = await axios.post("/user/create/post", {
+                title, description
+            })
+            console.log("DATA FROM CREATE POST HANDLER", data);
+            setLoading(false)
+        } catch(err) {
+            setLoading(false)
+            setError("Something went wrong")
+            console.error("ERROR OCCURRED", err)
+            setTimeout(() => {
+                setError("")
+            })
+        }
+    }
+
+    return {
+        error, loading,
+        setTitleHandler, setDescriptionHandler, createPostHandler
+    }
+}
+
+export default useCreatePost;
