c888e060f90c00ef819021f5618792ff1bdb8ade
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-13 07:29:11 +0100

    added RootState type and AppDispatch type

--- src/state-management/store/store.ts
+++ src/state-management/store/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from '../createslice/user'
-
+import postReducer from '../createslice/post'
 
 
 const store = configureStore({
     reducer: {
-        users: userReducer
+        users: userReducer,
+        posts: postReducer
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
 
 export default store;
