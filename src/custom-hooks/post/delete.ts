7fd73f777bf2f9988ba238251cbf390ff626eedf
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-13 06:49:03 +0100

    created the custom hook for deleteing a post

---
+++ src/custom-hooks/post/delete.ts
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import type { RootState } from '../../state-management/store/store'
+
+const useDelete = () => {
+    const [error, setError] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const navigate = useNavigate()
+    const pushPost = useSelector((p: RootState) => p.posts.pushPost)
+    const userState = useSelector((u: RootState) => u.users.user)
+
+    const deletePostHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(!pushPost.id) return
+        try {
+            setLoading(false)
+            const data = await axios.delete(`/user/delete/post/${userState.id}/${pushPost.id}`)
+            console.log("DATA FROM DELETE POST HANDLER", data)
+            setLoading(false)
+            navigate("/home");
+        } catch (err) {
+            setLoading(false)
+            setError("Something went wrong")
+            console.error("ERROR OCCURRED", err)
+            setTimeout(() => {
+                setError("")
+            })
+        }
+    }
+
+    return {
+        error, loading, 
+        deletePostHandler
+    }
+}
+
+export default useDelete;
