8aec17713d4f257de0c35b7229a3441e50b9cca3
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-13 06:47:41 +0100

    added login and registered lists

--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -1,16 +1,34 @@
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import type { RootState } from '../../state-management/store/store'
 
 import './header.css'
 
 
-const header = () => (
-    <ul className="headerContainer">
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/create">Create Post</NavLink>
-        <NavLink to="/delete">Delete Post</NavLink>
-        <NavLink to="/update">Update Post</NavLink>
-        <NavLink to="/logout">Logout</NavLink>
-    </ul>
-);
+interface headerProps {
+    userId: number | null
+}
 
-export default header;
+const Header: React.FC<headerProps> = ({ userId }) => {
+    const pushPost = useSelector((p: RootState) => p.posts.pushPost);
+    console.log("HEADER", pushPost)
+    return (
+        <div> 
+            {userId !== null ? <ul className="headerContainer">
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/create">Create Post</NavLink>
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to={`/delete`}>Delete Post</NavLink>
+                <NavLink to={`/update`}>Update Post</NavLink>
+                <NavLink to="/logout">Logout</NavLink>
+            </ul> :
+            <ul className="headerContainer">
+                <NavLink to="/">Login</NavLink>
+                <NavLink to="/signup">Register</NavLink>
+            </ul>}
+        </div>
+    );
+}
+
+export default Header;
