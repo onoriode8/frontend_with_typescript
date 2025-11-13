11ed96397c07d918041e35be49c58c5b4b831bb0
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-13 07:05:47 +0100

    extracted user state from redux

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,22 +2,27 @@ import { NavLink } from 'react-router-dom'
 import { MdCreate } from "react-icons/md";
 import { useSelector } from 'react-redux';
 
+import type { RootState } from '../../state-management/store/store';
+
 import './home.css'
 
 
 const Home = () => {
-    const userState = useSelector(u => u.users.user)
+    const userState = useSelector((u: RootState) => u.users.user)
+
     return (
-        <div className='home_container'>
-            <div>
-                <div>Welcome {userState.name}!</div>
-                <p>Please click on the right icon to create post 👉</p>
-            </div>
-            <div>
-                <NavLink to="/create">
-                    <MdCreate title="Create Post"/>
-                </NavLink>
-                <NavLink to="/create">Create Post</NavLink>
+        <div>
+            <div className='home_container'>
+                <div>
+                    <div>Welcome {userState.name}!</div>
+                    <p>Please click on the right icon to create post 👉</p>
+                </div>
+                <div>
+                    <NavLink to="/create">
+                        <MdCreate title="Create Post"/>
+                    </NavLink>
+                    <NavLink to="/create">Create Post</NavLink>
+                </div>
             </div>
         </div>
     );
