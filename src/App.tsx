1c5b6b12864dc2b1b04f03d685165e3cdc8f620a
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-13 06:32:21 +0100

    added some routes and component for listing posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,57 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
+import Loader from './pages/loader/spinner/spinner';
+import type { RootState } from './state-management/store/store';
 
 const Layout = React.lazy(() => import('./components/layout'))
 const Login = React.lazy(() => import("./pages/auth/login/login"))
 const Signup = React.lazy(() => import("./pages/auth/register/register"))
 const Home = React.lazy(() => import("./pages/home/home"))
+const Posts = React.lazy(() => import('./pages/post/Posts/posts'))
 const Create = React.lazy(() => import("./pages/post/create/create"))
 const Delete = React.lazy(() => import("./pages/post/delete/delete"))
 const Update = React.lazy(() => import("./pages/post/update/update"))
-
+const DeletePost = React.lazy(() => import("./pages/post/deletePost/deletePost"))
+const UpdatePost = React.lazy(() => import("./pages/post/updatePost/updatePost"))
 
 
 function App() {
+  const postId = useSelector((p: RootState) => p.posts.pushPost)
+  const user = useSelector((u: RootState) => u.users.user)
+
+        console.log("APP STATE", postId)
+  
   return (
     <React.Fragment>
-      <Layout />
-      <Routes>
+      <Layout userId={user.id} />
+      {user.id === null ? <Routes>
         <Route path="/" 
-          element={<Suspense fallback={<p>Loading...</p>}><Login /></Suspense>} />
+          element={<Suspense fallback={<Loader />}><Login /></Suspense>} />
         <Route path="/signup" 
-          element={<Suspense fallback={<p>Loading...</p>}><Signup /></Suspense>} />
+          element={<Suspense fallback={<Loader />}><Signup /></Suspense>} />
+      </Routes> :
+      <Routes>
         <Route path="/home" 
-          element={<Suspense fallback={<p>Loading...</p>}><Home /></Suspense>} />
+          element={<Suspense fallback={<Loader />}><Home /></Suspense>} />
+        <Route path={`/posts`}
+          element={<Suspense fallback={<Loader />}><Posts /></Suspense>} />
         <Route path="/create" 
-          element={<Suspense fallback={<p>Loading...</p>}><Create /></Suspense>} />
-        <Route path="/update" 
-          element={<Suspense fallback={<p>Loading...</p>}><Update /></Suspense>} />
-        <Route path="/delete" 
-          element={<Suspense fallback={<p>Loading...</p>}><Delete /></Suspense>} />
-      </Routes>
+          element={<Suspense fallback={<Loader />}><Create /></Suspense>} />
+        <Route path={`/delete`}
+          element={<Suspense fallback={<Loader />}><DeletePost /></Suspense>} />
+        <Route path={`/update`}
+          element={<Suspense fallback={<Loader />}><UpdatePost /></Suspense>} />
+
+        <Route path={`/update/${postId.id}`} 
+          element={<Suspense fallback={<Loader />}><Update /></Suspense>} />
+
+        <Route path={`/delete/${postId.id}`}
+          element={<Suspense fallback={<Loader />}><Delete /></Suspense>} />
+      </Routes>}
     </React.Fragment>
-  )
+  ) 
 }
 
 export default App;
