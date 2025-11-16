acff35147264d671fbdb0ab284b07d69f9c1aed4
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-16 15:51:07 +0100

    logic for props interface and displaying props data title and description

--- src/pages/post/delete/delete.tsx
+++ src/pages/post/delete/delete.tsx
@@ -1,20 +1,30 @@
+import Spinner from '../../loader/spinner/spinner'
 import useDelete from '../../../custom-hooks/post/delete'
 
 import './delete.css'
 
+interface Props {
+  pushPost: {
+    id: number
+    title: string,
+    description: string
+  }
+}
 
-const DeletePost = () => {
+const DeletePost = (props: Props) => {
     const { error, loading, deletePostHandler } = useDelete()
     return (
         <div>
-            <div className="deletePost_container">
+            {error.length !== 0 ? <p style={{textAlign: "center"}}>{error}</p> : null }
+            {loading && <Spinner /> }
+            <form className="deletePost_container" onSubmit={(e) => deletePostHandler(e)}>
                 <p>You sure you want to delete ?</p>
                 <div>
-                    <p>{}</p>
-                    <p>{}</p>
+                    <p>Title: {props.pushPost.title}</p>
+                    <p>Description: {props.pushPost.description}</p>
                 </div>
                 <button type="submit">Delete Post</button>
-            </div>
+            </form>
         </div>
     );
 }
