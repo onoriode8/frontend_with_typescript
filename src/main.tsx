173916b857e327a816215623fed9edda3c07d3ae
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 09:42:43 +0100

    added reducer store with redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import { Provider } from 'react-redux'
+import { Provider, useSelector } from 'react-redux'
 import App from './App.tsx'
 
 import store from './state-management/store/store.ts'
+import type { RootState } from './state-management/store/store';
+
+export default function Render() {
+  const user = useSelector((u: RootState) => u.users.user)
+}
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <App pass={Render}/>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
