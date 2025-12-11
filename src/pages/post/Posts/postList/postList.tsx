242edb4cf68c6c182c4724f9568a0e91b9f13a6a
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-11 10:53:39 +0100

    bug fix

--- src/pages/post/Posts/postList/postList.tsx
+++ src/pages/post/Posts/postList/postList.tsx
@@ -5,25 +5,27 @@ import { MdDelete } from "react-icons/md";
 import './postList.css'
 
 
-interface PostListProps {
-    id: number,
-    title: string | null,
-    description: string | null,
-    pushIdToReducerHandler: (id: number, path: string) => void
-}
+// interface PostListProps {
+//     id: number,
+//     title: string | null,
+//     description: string | null,
+//     pushIdToReducerHandler: (id: number, path: string) => void
+// } : React.FC<PostListProps>
 
-const PostList: React.FC<PostListProps> = (props) => {
-    console.log("POSTLIST", props)
+const PostList = ({ id, title, description, pushIdToReducerHandler }) => {
+    console.log("POSTLIST", id, title, description, pushIdToReducerHandler)
+
     return (
         <div>
-            {props.id && <div className="postList_container">
+            {/* <p style={{marginTop: "500px"}}>HELLO POSTS</p> */}
+            {id && <div className="postList_container">
                 <div>
-                    <p>{props.title}</p>
-                    <p>{props.description}</p>
+                    <p>{title}</p>
+                    <p>{description}</p>
                 </div>
                 <div>
-                    <MdCreate onClick={() => props.pushIdToReducerHandler(props.id, '/update')} />
-                    <MdDelete onClick={() => props.pushIdToReducerHandler(props.id, '/delete')} />
+                    {/* <MdCreate onClick={() => pushIdToReducerHandler(id, '/update')} />
+                    <MdDelete onClick={() => pushIdToReducerHandler(id, '/delete')} /> */}
                 </div>
             </div>}
         </div>
